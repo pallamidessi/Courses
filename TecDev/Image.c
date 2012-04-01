8bd5f9aa7ab6eb92e6ae6b6b6e1f93ede819289d
Pallamidessi joseph <sangdragon777@gmail.com>
2012-04-01 15:25:48 +0200

    fonction chargement image terminee

--- TecDev/Image.c
+++ TecDev/Image.c
@@ -16,18 +16,18 @@
 int Seuil(char* nom_du_fichier){
 int i=0;
 FILE* fichier = NULL;
-int sizeImageH=0,int sizeImageV=0;
+int sizeImageH=0,sizeImageV=0;
 char R=0,V=0,B=0;
 int total=0,seuil=0;
 int ValMax=0;
-
+char chaine[TAILLE_MAX];
 fichier = fopen(nom_du_fichier, "r");
 
   if (fichier != NULL)
   {
-    fgets(mode,TAILLE_MAX,fichier);
     fgets(chaine,TAILLE_MAX,fichier);
-    fscanf(fichier,"%s %s", &sizeImageH,&sizeImageV);
+    fgets(chaine,TAILLE_MAX,fichier);
+    fscanf(fichier,"%d %d", &sizeImageH,&sizeImageV);
     fscanf(fichier,"%d", &ValMax);
 
      for(i=0;i<(sizeImageV*sizeImageH);i++){
@@ -44,31 +44,32 @@ return seuil;
 
 
 
-grille charger_image(grille L, char* nom_du_fichier,char* mode,int seuil){
+grille charger_image( char* nom_du_fichier,char* mode,int seuil){
 int i=0,v=0;
 char R=0,V=0,B=0;
 FILE* fichier = NULL;
 int ValMax=0;
 char modeImage[2];
-int sizeImageH=0,int sizeImageV=0;
+int sizeImageH=0, sizeImageV=0;
 char chaine[TAILLE_MAX] = "";
+grille charge;
 
-fichier = fopen(nom, mode);
+fichier = fopen(nom_du_fichier, mode);
 
 	if (fichier != NULL)
   {
 
-fgets(modeImage,TAILLE_MAX,fichier);             //1er ligne le mode
-fgets(chaine,TAILLE_MAX,fichier);                 //2eme ligne meta donnee de creation de l'image -> On passe simplement a la ligne suivante
-fscanf(fichier,"%s %s", &sizeImageH,&sizeImageV); //3eme ligne recuperation de la taille de l'image
-fscanf(fichier,"%d", &ValMax);                     //4eme valeurs max des composantes
+fgets(modeImage,TAILLE_MAX,fichier);                    //1er ligne le mode
+fgets(chaine,TAILLE_MAX,fichier);                       //2eme ligne meta donnee de creation de l'image -> On passe simplement a la ligne suivante
+fscanf(fichier,"%d %d", &sizeImageH,&sizeImageV);       //3eme ligne recuperation de la taille de l'image
+fscanf(fichier,"%d", &ValMax);                          //4eme valeurs max des composantes
 
-if (sizeImageH>70)                                 // tronque l'image si elle est plus grande que 70 colonneS
+if (sizeImageH>70)                                      // tronque l'image si elle est plus grande que 70 colonneS
     sizeImageH=70;
 
-grille charge=alloue_grille(sizeImageH,sizeImageV);     // creer un logigraphe a la taille de l'image
+charge=alloue_grille(sizeImageH,sizeImageV);     // creer un logigraphe a la taille de l'image
 
- if (modeImage=="P3"){
+ //if (modeImage=="P3"){
 
     for(i=0;i<sizeImageV;i++){
         for(v=0;v<sizeImageH;v++){
@@ -76,16 +77,18 @@ grille charge=alloue_grille(sizeImageH,sizeImageV);     // creer un logigraphe a
             fscanf(fichier,"%c", &V);
             fscanf(fichier,"%c", &B);
 
-            if((R*0.299+V*0.587+B*0.114)>=seuil)
-                charge[i][v]='+';
+            if((R*0.299+V*0.587+B*0.114)<=seuil)
+                charge->matrice[i][v]='+';
             else
-                charge[i][v]='.';
+                charge->matrice[i][v]='.';
 
         }
     }
- }
+ //}
 
   	 fclose(fichier);
   }
 return charge;
 }
+
+
