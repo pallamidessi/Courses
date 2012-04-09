88abb61041ba17ea0094908e3194be40a591ec21
Pallamidessi joseph <sangdragon777@gmail.com>
2012-04-09 23:29:08 +0200

    header du .c eponyme

---
+++ TecDev/header/cairo_util.h
@@ -0,0 +1,12 @@
+#ifndef _grille_util_h
+#define _grille_util_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "grille_cairo.h"
+
+cairo_t* tracer_grille(cairo_surface_t* surface,grille l,int decalX,int decalY);
+
+
+
+#endif
