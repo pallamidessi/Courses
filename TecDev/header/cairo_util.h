998472eaf2867012e11e4141ed81c336b6f56052
Pallamidessi joseph <sangdragon777@gmail.com>
2012-04-11 12:08:28 +0200

    ajout structure rectangle

--- TecDev/header/cairo_util.h
+++ TecDev/header/cairo_util.h
@@ -4,9 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "grille_cairo.h"
+typedef struct _rectangle{
+int x;
+int y;
+int width;
+int height;
+} rectangle_t;
 
-cairo_t* tracer_grille(cairo_surface_t* surface,grille l,int decalX,int decalY);
-
+void Affiche_jeu(cairo_surface_t* surface,grille l,grille Ligne,grille Col,int decalX,int decalY,int Sx,int Sy);
+void carreSelection(cairo_t* mask,int Sx,int Sy,grille l,int decalX,int decalY);
 
 
 #endif
