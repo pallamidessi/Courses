3cf69a95f1f2953847aea85fcd51134867d16e72
Pallamidessi <joseph.pallamidessi@gmail.com>
2012-10-02 19:10:57 +0200

    fin tp1

--- PSR/TP1/exo3.c
+++ PSR/TP1/exo3.c
@@ -1,3 +1,15 @@
+
+/**
+ * \file			exo3.c
+ * \author		Pallamidessi joseph
+ * \version		1.0 
+ * \date			2 octobre 2012
+ * \brief			Creer un fichier (ou ecrase un fichier existant) de la taille souhaitee.
+ * 
+ * \details	
+ * 
+ */ 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -10,18 +22,18 @@
 
 int main (int argc , char* argv[] ){
 				
-int fd;
-int truc;
+	int fd;
+	int truc;
 
-if (argc != 3){
-	printf("usage : on donne le nom et lq taille du fichier que l'on souhaite");
-	exit(1);
+	if (argc != 3){
+		printf("usage : on donne le nom et lq taille du fichier que l'on souhaite");
+		exit(1);
 	}
-fd=open(argv[1],O_CREAT|O_TRUNC|O_WRONLY,0777);
+	fd=open(argv[1],O_CREAT|O_TRUNC|O_WRONLY,0777);
 
-write(fd,&truc,atoi(argv[2]));
+	write(fd,&truc,atoi(argv[2]));
 
-close(fd);
+	close(fd);
 
 exit(0);
 }
