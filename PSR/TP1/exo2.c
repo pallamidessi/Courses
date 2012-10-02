f19af4fd58884b27a4ad03f888414dac06ba2ae7
Pallamidessi <joseph.pallamidessi@gmail.com>
2012-10-02 19:10:47 +0200

    fin tp1

--- PSR/TP1/exo2.c
+++ PSR/TP1/exo2.c
@@ -1,3 +1,16 @@
+
+/**
+ * \file			exo2.c
+ * \author		Pallamidessi joseph
+ * \version		1.0 
+ * \date			2 octobre 2012
+ * \brief			On creer un fichier (ou on ecrase le fichier existant), et on ecrit 10 int
+ * dedans
+ * 
+ * \details		
+ * 
+ */ 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -13,19 +26,19 @@
 
 int main (int argc , char* argv[] ){
 
-int fd;
-int buffer;
-int i=0;
+	int fd;
+	int buffer;
+	int i=0;
 
 
-fd=open("test.txt",O_CREAT|O_TRUNC|O_WRONLY,0777);
+	fd=open("test.txt",O_CREAT|O_TRUNC|O_WRONLY,0777);
 
-for(i=0;i<10;i++){
-	scanf("%d",&buffer);
-	write(fd,&buffer,4);	
+	for(i=0;i<10;i++){
+		scanf("%d",&buffer);
+		write(fd,&buffer,4);	
 	}
 
-close(fd);
-exit(0);
+	close(fd);
+	exit(0);
 }
 				
