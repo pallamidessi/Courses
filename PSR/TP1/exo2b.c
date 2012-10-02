8a0711cac6d14c46576e5cba4aa96c9c8c626494
Pallamidessi <joseph.pallamidessi@gmail.com>
2012-10-02 19:10:51 +0200

    fin tp1

--- PSR/TP1/exo2b.c
+++ PSR/TP1/exo2b.c
@@ -1,4 +1,15 @@
 
+/**
+ * \file			exo2b.c
+ * \author		Pallamidessi joseph
+ * \version		1.0 
+ * \date			2 octobre 2012
+ * \brief			On lit le fichier binaire cree pour l'exo2	
+ * 
+ * \details		
+ * 
+ */ 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -24,7 +35,7 @@ if ((fd=open("test.txt",O_RDONLY))==-1){
 }
 
 for(i=0;i<10;i++){
-	read(fd,&buffer,4);
+	read(fd,&buffer,4);			// On lit par pack de 4 octets (sizeof(int))	
 	printf("%d\n",buffer);
 	}
 
