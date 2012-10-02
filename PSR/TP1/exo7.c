2850da28b128a507b5e6a6e613c3bd47a14ea90d
Pallamidessi <joseph.pallamidessi@gmail.com>
2012-10-02 19:11:14 +0200

    fin tp1

--- PSR/TP1/exo7.c
+++ PSR/TP1/exo7.c
@@ -1,3 +1,15 @@
+
+/**
+ * \file			exo7.c
+ * \author		Pallamidessi joseph
+ * \version		1.0 
+ * \date			2 octobre 2012
+ * \brief			On ecrit sur la sortie standard le contenu d'un fichier donne en argument.
+ * 
+ * \details		
+ * 
+ */ 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -9,25 +21,25 @@
 
 int main (int argc , char* argv[] ){
 
-int fd;
-int i=0;
-char buffer;
+	int fd;
+	int i=0;
+	char buffer;
 
-if (argc!=2){
-	printf("usage: nombre d'argument\n");
-	exit(1);
-}
+	if (argc!=2){
+		printf("usage: nombre d'argument\n");
+		exit(1);
+	}
 
-if ((fd=open(argv[1],O_RDONLY))==-1){
-	printf("le fichier n'existe pas ou probleme de droit");
-	exit(2);
-} 
+	if ((fd=open(argv[1],O_RDONLY))==-1){
+		printf("le fichier n'existe pas ou probleme de droit");
+		exit(2);
+	} 
 
-while(lseek(fd,-i, SEEK_END)!=-1){
-	read(fd,&buffer,1);
-	write(1,&buffer,1);				
-	i++;
-}
+	while(lseek(fd,-i, SEEK_END)!=-1){
+		read(fd,&buffer,1);
+		write(1,&buffer,1);				
+		i++;
+	}
 
-return 0;
+	return 0;
 }
