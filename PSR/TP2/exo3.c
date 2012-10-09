e7aee184af0395613bc750a1bb60aa806d87b5ca
shrimp <shrimp@debian.localdomain>
2012-10-09 20:00:57 +0200

    exo3 fini et exo5 en cours

---
+++ PSR/TP2/exo3.c
@@ -0,0 +1,59 @@
+
+
+/**
+ * \file			exo2.c
+ * \author		Pallamidessi joseph
+ * \version		1.0 
+ * \date			3 octobre 2012
+ * \brief		
+ * 
+ * \details		
+ * 
+ */ 
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int args,char* argv[])
+{
+	int n=0,m=0,pid;
+	int i=0,j=0;
+	int status;
+
+	if(args !=3){
+		perror("usage:nombre d'argument ");
+		exit(1);
+	}
+	
+	n=atoi(argv[1]);
+	m=atoi(argv[2]);
+
+	for(j=0;j<m;j++){	
+		pid=fork();		
+		switch (pid){
+			case -1 :
+							perror("erreur fork");
+							exit(2);
+			case	0	: 
+							for(i=0;i<n;i++){
+								if(fork())
+									wait(&status);
+								else
+									break;
+							}
+							printf("proc %d pere %d\n",getpid(),getppid());
+							exit(0);
+			default : 		
+								wait(&status);
+	
+		}
+	}
+	return 0;
+																																								 
+}
--- PSR/TP2/exo5.c
+++ PSR/TP2/exo5.c
@@ -1,14 +1,4 @@
-
-/**
- * \file			exo2.c
- * \author		Pallamidessi joseph
- * \version		1.0 
- * \date			3 octobre 2012
- * \brief		
- * 
- * \details		
- * 
- */ 
+#include <math.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
@@ -19,32 +9,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+typedef struct Liste{
+	int *l;
+	int nbrElem;
+	int taille;
+
+}str_liste,*liste;
+
+liste agrandissement(liste a){
+	realloc(a->l,a->taille+10*sizeof(int));
+	a->taille+=10;
+	return a;
+	}
+
+int* generateur(n){
+	liste listeNbr=(liste)malloc(sizeof(str_liste));
 	
-	int pipefd[2],n;
+	listeNbr->taille=(int)floor(pow(n,2))-1;
+	int n2=listeNbr->taille;
+	int i;
+	listeNbr->l=(int*) malloc(n2*sizeof(int));
+
+	liste->nbrElem=n2;
+
+	for(i=0;i<n2;i++){
+		liste[i]=i+1;
+		}
+	return listeNbr;
+	}
+
+
+int* diviseur(liste aDiviser,int d){
+	liste nouvliste;
+	
+
+	for(i=0;i<aDiviser->taille;i++){
+		if(aDiviser->l[i]%d!=0)
+			if 
+		
+		
+		}
+	
+	}
+int main(int args,char* argv[])
+{
+	int n=0,m=0,pid;
+	int i=0;
+	int status;
+  int pipefd;
 
 	if(args !=2){
 		perror("usage:nombre d'argument ");
 		exit(1);
 	}
-
-	n=atoi(argv[1]);
 	
-	for(i=0;i<n;i++){	
-		pipe(pipefd);
-		int m=1;
-		pid=fork();
-			
+	n=atoi(argv[1]);
 
-			switch (pid){
-				case -1 :
-								perror("erreur fork");
-								exit(2);
-				case	0	: write()
-								
-				default : for (i=0;i<n;i++){
-										printf("%d ",i);
-										exit(0);
-									}
-			}
+
+pid=fork();
+
+		pid=fork();		
+		switch (pid){
+			case -1 :
+							perror("erreur fork");
+							exit(2);
+			case	0	:
+							
+							for(i=0;i<n;i++){
+								if(fork())
+									wait(&status);
+								else
+									break;
+							}
+			default : 		
+								wait(&status);
+	
 		}
+	}
