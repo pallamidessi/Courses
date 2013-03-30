eb5364385cad9b3ee86a0e2cf8405f0c4a3ecbe0
Pallamidessi <joseph.pallamidessi@gmail.com>
2013-03-30 17:15:51 +0100

    Exercice 3: code polynomiaux fini

--- RP/TP3/CodeCorrecteur/coding.c
+++ RP/TP3/CodeCorrecteur/coding.c
@@ -44,7 +44,7 @@ void copyDataBitsCoding(char *message, CodeWord_t *cw, int size)
 }
 
 
-
+/*
 void computeCtrlBits(CodeWord_t *message, int size)
 {
 	int i,j;
@@ -59,12 +59,55 @@ void computeCtrlBits(CodeWord_t *message, int size)
 	}
 	return;
 }
+*/
 
+void computeCtrlBits(CodeWord_t *message, int size)
+{
+	int i = 0;	
+	int j = 0;	
+	int k = 0;	
+
+	int X[9];
+	
+	for(i=0;i<9;i++){
+		X[i]=0;
+	}
+
+	CodeWord_t* toCompute;
+	int entree=0;
+
+	for(i=0;i<size/(int) sizeof(CodeWord_t);i++){
+		toCompute=&message[i];
+		
+		for(k=0;k<9;k++){
+			X[k]=0;
+		}
+
+		for(j=0;j<8;j++){
+			entree=getNthBit(*toCompute,8+j);
+
+			X[8]=X[7];		
+			X[7]=X[6];		
+			X[6]=X[5];		
+			X[5]=X[4];		
+			X[4]=X[3]^X[8];		
+			X[3]=X[2]^X[8];		
+			X[2]=X[0]^X[8];		
+			X[1]=X[0];		
+			X[0]=entree^X[8];		
+		}
+		for(j=0;j<8;j++){
+			setNthBitCW(toCompute,9+j,X[j]);	
+		}
+	}
+	
+	return;
+}
 
 void coding(char *message, int data_size, char *cw, int *cw_size)
 {
   *(cw_size)= data_size * sizeof(CodeWord_t);
-	int i;
+	//int i;
 
   copyDataBitsCoding(message, (CodeWord_t*)cw, data_size);
 	
