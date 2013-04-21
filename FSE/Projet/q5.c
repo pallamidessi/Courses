18482a285dc0368500efe118ea0d31599982c158
Pallamidessi <joseph.pallamidessi@gmail.com>
2013-04-21 18:33:28 +0200

    final

--- FSE/Projet/q5.c
+++ FSE/Projet/q5.c
@@ -1,38 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <sys/stat.h>
 
 #include "e2fs.h"
 
 #define	MAXBUF	100
 
-/* Affiche le fichier reference par l'inode avec l'"ouverture de fichier" */
 
+/* Affiche le fichier reference par l'inode avec l'"ouverture de fichier" */
 int main (int argc, char *argv [])
 {
     ctxt_t c ;
 
     if (argc != 3)
     {
-	fprintf (stderr, "usage: %s fs path\n", argv [0]) ;
-	exit (1) ;
+			fprintf (stderr, "usage: %s fs inode\n", argv [0]) ;
+			exit (1) ;
     }
 
     c = e2_ctxt_init (argv [1], MAXBUF) ;
     if (c == NULL)
     {
-	perror ("e2_ctxt_init") ;
-	exit (1) ;
+			perror ("e2_ctxt_init") ;
+			exit (1) ;
     }
 		
-		int nb_inode=e2_namei(c,argv[2]);
+		char last_char;
+
+    file_t f=e2_file_open(c,atoi(argv[2]));
+
+		while(( last_char = e2_file_getc(f) ) != -1 ){
+			printf("%c",last_char);
+
+		}	
 		
-		printf("inode %d \n",nb_inode);
-
-		//if (S_ISDIR()) {
-			/* code */
-		//}
-    /* A REDIGER */
-
+		printf("\n");
+		e2_file_close(f);
     e2_ctxt_close (c) ;
 
     exit (0) ;
--- FSE/Projet/q7.c
+++ FSE/Projet/q7.c
@@ -1,41 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <sys/stat.h>
 
 #include "e2fs.h"
 
 #define	MAXBUF	100
 
-/* Affiche le fichier ou le repertoire reference par l'inode */
 
+/* Affiche le fichier ou le repertoire reference par l'inode */
 int main (int argc, char *argv [])
 {
     ctxt_t c ;
 
     if (argc != 3)
     {
-	fprintf (stderr, "usage: %s fs chemin\n", argv [0]) ;
-	exit (1) ;
+			fprintf (stderr, "usage: %s fs chemin\n", argv [0]) ;
+			exit (1) ;
     }
 
     c = e2_ctxt_init (argv [1], MAXBUF) ;
     if (c == NULL)
     {
-	perror ("e2_ctxt_init") ;
-	exit (1) ;
+			perror ("e2_ctxt_init") ;
+			exit (1) ;
     }
 		
-		char last_char;
+		int nb_inode=e2_namei(c,argv[2]);
+		
+		printf("inode %d \n",nb_inode);
 
-    file_t f=e2_file_open(c,atoi(argv[2]));
-
-		while(( last_char = e2_file_getc(f) ) != -1 ){
-			printf("%c",last_char);
-
-		}	
-printf("\n");
-		e2_file_close(f);
     e2_ctxt_close (c) ;
 
     exit (0) ;
