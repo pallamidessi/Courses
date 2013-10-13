90a75867dc6f666782a1a8f5503e820a6f4bb65b
Pallamidessi <pallamidessi@etu.unistra.fr>
2013-10-13 17:36:15 +0200

    fin conflit

--- IHM/TP1/dialogs.cpp
+++ IHM/TP1/dialogs.cpp
@@ -11,12 +11,8 @@ VersionDialog::VersionDialog( wxWindow *parent, wxWindowID id,
 	wxStaticText* item1=new wxStaticText(this,ID_TEXT,wxT("Version 1.0"),wxDefaultPosition,wxDefaultSize,wxALIGN_CENTRE);
 	wxButton* item2=new wxButton(this,wxID_OK,wxT("OK"),wxDefaultPosition);
 
-	item0->Add(item1,0,wxALIGN_CENTRE|wxALL,5);
-<<<<<<< HEAD
 	item0->Add(item2,0,wxALIGN_CENTRE|wxALL,5);
 	item0->Add(item2,0,wxALIGN_CENTRE|wxALL,5);
-=======
->>>>>>> 2f7fa52b62714b1f0d81bff852e0c48761d7f503
 
 	this->SetAutoLayout(TRUE);
 	this->SetSizer(item0);
