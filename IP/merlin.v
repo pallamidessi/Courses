8aa22514f6263f339f09affdcab62ed7e3aefd7c
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2015-04-11 19:07:13 +0200

    Added basics

---
+++ IP/merlin.v
@@ -0,0 +1,19 @@
+Require Import Bool.
+Require Import List.
+Require Import NArith.
+Require Import Omega.
+
+Definition couple := (nat * nat)%type.
+
+(*
+
+negb               negation sur les booleens
+Even.even_odd_dec  test de parite
+count_occ          compteur d’occurences
+++                 concatenation sur les listes
+length longueur d’une liste
+fold left voir doc
+On pourra utiliser les tactiques : destruct, omega, . . .
+On pourra utiliser les lemmes : negb involutive, . . .
+
+*)
